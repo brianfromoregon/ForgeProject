51200b9036fe1b98f8ab9ed555c6d3fdf300cc7d
Brian Harris <brianfromoregon@gmail.com>
2015-10-01 17:03:03 -0700

    Add a list of turtle challenges

---
+++ src/main/java/com/findrealhope/examples/TurtleChallenges.java
@@ -0,0 +1,53 @@
+package com.findrealhope.examples;
+
+/**
+ * Challenges!
+ *
+ * - Can you tell the turtle to draw a box? That would be a nice start to a house!
+ *
+ * - Can you tell your turtle to erase blocks instead of create them?
+ *   Hint: there is a block type called "Blocks.air"
+ *
+ *
+ *
+ * Advanced math challenges
+ * - The turtle is good at drawing lines that go east to west, north to south, or up to down.
+ *   But how about a line that goes NE to SW? Or a line that goes exactly in the direction you are facing?
+ *   Hint: search youtube for Bresenham
+ *
+ * - Can you tell the turtle how to draw a circle? Curves are hard in a blocky world.... A computer screen
+ *   is also made up of blocks (pixels) so how do they manage to draw curves?
+ *   Hint: search youtube for midpoint circle algorithm
+ *
+ * - Other tricky shapes: cylinder, prism (a prism would be handy for making roofs!), sphere
+ *
+ * - Tell the turtle to plot a function such as sin(x)*2.
+ *
+ *
+ *
+ * Artistic challenge
+ * - We don't get to see the turtle sadly..... it's only an imaginary turtle... ;(
+ *   So fix that! Write a mod that adds a new creature to the game: a cute little turtle.
+ *
+ *
+ *
+ *
+ *
+ * Very hard expert level Java programming challenge
+ * - Make a new type of turtle which draws slowly, one block per second, so you can actually see
+ *   the blocks getting created in the game one at a time instead of all at once.
+ *
+ *
+ *
+ *
+ *
+ * More ideas
+ * - Generate random mazes you can walk through.
+ * - Draw words in the sky
+ * - Draw a fractal
+ * - Draw Langton's Ant
+ *
+ */
+public class TurtleChallenges {
+
+}
