58244bce5ecf9ecb694a5298177a2be29b0dbdf4
Brian Harris <brianfromoregon@gmail.com>
2015-10-14 18:58:12 -0700

    update modid

--- src/main/java/MainMod.java
+++ src/main/java/MainMod.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 
 @Mod(modid = MainMod.MODID, version = MainMod.VERSION)
 public class MainMod {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "abccoders";
     public static final String VERSION = "1.0";
 
     @EventHandler
